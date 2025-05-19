f94a80f5ff968a0a1d3ee5229bde382fe1cc5bea
Supreet0 <supreetpatel2004@gmail.com>
2025-05-19 18:42:40 +0530

    added geolocation hook in it

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,5 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { useGeoloacation } from "@/hooks/use-geolocation";
+import { RefreshCw } from "lucide-react";
+
 const WeatherDashboard = () => {
-  return <div></div>;
+  const { coordinates, error, getLocation, isLoading } = useGeoloacation();
+  console.log(coordinates);
+
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          //onClick={handleRefresh}
+          //disabled={}
+        >
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherDashboard;
