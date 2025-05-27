f504a3a92efb4f58c500bce2ea4f3f69da10cd33
Supreet0 <supreetpatel2004@gmail.com>
2025-05-27 17:53:39 +0530

    added the current weather dashboard

---
+++ src/components/hourly-temperature.tsx
@@ -0,0 +1,11 @@
+import type { ForecastData } from "@/api/types";
+
+interface HourlyTemperatureProps {
+  data: ForecastData;
+}
+
+const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
+  return <div></div>;
+};
+
+export default HourlyTemperature;
